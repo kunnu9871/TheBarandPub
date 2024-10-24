76867cf852a53582d0dfeed620cf50ade15e29b3
Kunal Singh <126372340+kunnu9871@users.noreply.github.com>
2024-10-24 19:05:21 +0530

    Update Cart.jsx

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -27,9 +27,11 @@ const Cart = () => {
             <p className="text-2xl font-semibold mb-4">Your cart is empty.</p>
             <button
               onClick={() => navigate("/")}
+              onClick={() => navigate("/menu")}
               className="my-10 rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
             >
               Continue Shopping
+              Place order
             </button>
           </div>
         ) : (
